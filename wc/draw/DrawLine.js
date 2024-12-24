029b45368691a83fb8fc26021742af30e847eae6
mins01 <mins01.lycos.co.kr@gmail.com>
2024-12-24 11:26:21 +0900

    DrawLine 에 주석

--- wc/draw/DrawLine.js
+++ wc/draw/DrawLine.js
@@ -1,4 +1,13 @@
 export default class DrawLine{
+
+    /**
+     * 두 점으로 선을 그린다.
+     * @param {CanvasRenderingContext2D} ctx 
+     * @param {number} x0 
+     * @param {number} y0 
+     * @param {number} x 
+     * @param {number} y 
+     */
     static draw(ctx,x0,y0,x,y){
         ctx.beginPath();
         ctx.moveTo(x0,y0 )
@@ -8,13 +17,11 @@ export default class DrawLine{
     }
 
     /**
-     * 
-     * @param {*} ctx 
-     * @param {Array} coordinates  [x0,y0,x1,y1,x2,y2...]
+     * 연속된 좌표로 연속된 선을 그린다.
+     * @param {CanvasRenderingContext2D} ctx 
+     * @param {Array<number>} coordinates  [x0,y0,x1,y1,x2,y2...]
      */
-    static drawByCoordinates(ctx,coordinates){
-        console.log(ctx);
-        
+    static drawByCoordinates(ctx,coordinates){       
         if(!coordinates || !coordinates.length){ return false; }
         ctx.beginPath();
         ctx.moveTo(coordinates[0],coordinates[1])
