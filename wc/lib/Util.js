1eb903e8391488f94acfa303ef87f83c66d06647
mins01 <mins01.lycos.co.kr@gmail.com>
2025-01-07 14:16:58 +0900

    parseCssSize

--- wc/lib/Util.js
+++ wc/lib/Util.js
@@ -15,4 +15,41 @@ export default class Util{
         });
         return obj;
     }
+
+
+    static parseCssSize(value){
+        const vhr = value.match(/([-+]?(?:\d*\.?\d+|\d+\.?\d*)(?:[eE][-+]?\d+)?)([^\d]+)?/);
+        const number = vhr?.[1]??null;
+        const unit = vhr?.[2]??null;
+        return { number , unit };
+    }
+    static calculateSizeBasedOnFontSize(value,fontSize){
+
+        // this.parseCssSize(value)
+
+        const vhr = value.matchAll(/([-+]?(?:\d*\.?\d+|\d+\.?\d*)(?:[eE][-+]?\d+)?)([^\d]+)?/g);
+        const vhrs = [...vhr]; if(!vhrs || !vhrs[0]){ return null}
+        const valueNumber = parseFloat(vhrs[0][1]);
+        const valueUnit = (vhrs[0][2]??null);
+        const fsr = fontSize.matchAll(/([-+]?(?:\d*\.?\d+|\d+\.?\d*)(?:[eE][-+]?\d+)?)([^\d]+)?/g);
+        const fsrs = [...fsr]; if(!fsrs || !fsrs[0]){ return null}
+        const fontSizeNumber = parseFloat(fsrs[0][1]);
+        const fontSizeUnit = (fsrs[0][2]??null);
+
+        let r = '';
+        if(!valueUnit){
+            r = (valueNumber*fontSizeNumber)+fontSizeUnit;
+        }else if(valueUnit=='em'){
+            r = (valueNumber*fontSizeNumber)+fontSizeUnit;
+        }else if(valueUnit=='%'){
+            r = (valueNumber*fontSizeNumber/100)+fontSizeUnit;
+        }else{
+            r = value;
+        }
+        return r;
+    }
+
+    static convertToPx(v){
+
+    }
 }
