4a39a43a8ce53f1464ca7da4a11406dba163515a
공대여자 <mins01.lycos.co.kr@gmail.com>
2025-01-10 01:25:45 +0900

    wc/lib/CssFontUtil.js parseFont 동작 순서 변경

--- wc/lib/CssFontUtil.js
+++ wc/lib/CssFontUtil.js
@@ -33,31 +33,34 @@ export default class CssFontUtil{
             lineHeight:'',
             fontFamily:'',
         }
-        const regexpFontSize = /((?:[-+]?(?:\d*\.?\d+|\d+\.?\d*)(?:[eE][-+]?\d+)?)(?!deg)(?:[a-z]{2,3}|%)|xx-small|x-small|small|medium|large|x-large|xx-large|xxx-large|smaller|larger)(?!-)(?:\/((?:[-+]?(?:\d*\.?\d+|\d+\.?\d*)(?:[eE][-+]?\d+)?)(?:[a-z]{2,3}|%)?|normal))?(?:\s+)?(.*)?$/
-        const match1 = font.match(regexpFontSize);
+        let fontWork = font;
+        const regexpFontStyle = /(italic|oblique(?:\s+\d+deg))/;
+        let match2 = fontWork.match(regexpFontStyle);
+        if(match2) rs.fontStyle = match2[1]??'';
+        if(rs.fontStyle.length){ fontWork = fontWork.replace(rs.fontStyle,'') } 
+
+        const regexpFontVariant = /(small-caps)/;
+        match2 = fontWork.match(regexpFontVariant);
+        if(match2) rs.fontVariant = match2[1]??'';
+        if(rs.fontVariant.length){ fontWork = fontWork.replace(rs.fontVariant,'') } 
+
+        const regexpFontStretch = /(ultra-condensed|extra-condensed|condensed|semi-condensed|semi-expanded|expanded|extra-expanded|ultra-expanded)/;
+        match2 = fontWork.match(regexpFontStretch);
+        if(match2) rs.fontStretch = match2[1]??'';
+        if(rs.fontStretch.length){ fontWork = fontWork.replace(rs.fontStretch,'') } 
+
+        const regexpFontWeight = /(\d{1,4}|bold|bolder|lighter)/;
+        match2 = fontWork.match(regexpFontWeight);
+        if(match2) rs.fontWeight = match2[1]??'';
+        if(rs.fontWeight.length){ fontWork = fontWork.replace(rs.fontWeight,'') } 
+
+        const regexpFontSize = /((?:[-+]?(?:\d*\.?\d+|\d+\.?\d*)(?:[eE][-+]?\d+)?)(?:[a-z]{2,3}|%)|xx-small|x-small|small|medium|large|x-large|xx-large|xxx-large|smaller|larger)(?:\/((?:[-+]?(?:\d*\.?\d+|\d+\.?\d*)(?:[eE][-+]?\d+)?)(?:[a-z]{2,3}|%)?|normal))?(?:\s+)?(.*)?$/
+        const match1 = fontWork.match(regexpFontSize);
         if(!match1){ return null;}
         rs.fontSize = match1[1]??'';
         rs.lineHeight = match1[2]??'';
         rs.fontFamily = match1[3]??'';
         if(rs.fontSize=='' ||rs.fontFamily==''){ return null;} //필수값
-        
-        const fontPre = font.substring(0,match1.index-1);
-        
-        const regexpFontStyle = /(italic|oblique(?:\s+\d+deg))/;
-        let match2 = fontPre.match(regexpFontStyle);
-        if(match2) rs.fontStyle = match2[1]??'';
-
-        const regexpFontVariant = /(small-caps)/;
-        match2 = fontPre.match(regexpFontVariant);
-        if(match2) rs.fontVariant = match2[1]??'';
-
-        const regexpFontStretch = /(ultra-condensed|extra-condensed|condensed|semi-condensed|semi-expanded|expanded|extra-expanded|ultra-expanded)/;
-        match2 = fontPre.match(regexpFontStretch);
-        if(match2) rs.fontStretch = match2[1]??'';
-
-        const regexpFontWeight = /(\d{1,4}|bold|bolder|lighter)/;
-        match2 = fontPre.match(regexpFontWeight);
-        if(match2) rs.fontWeight = match2[1]??'';
 
         return rs;
     }
