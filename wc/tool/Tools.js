5a588c002b89847d22d898427159c5f76c958cbb
공대여자 <mins01.lycos.co.kr@gmail.com>
2025-01-13 23:59:11 +0900

    브러시 관련 개선

--- wc/tool/Tools.js
+++ wc/tool/Tools.js
@@ -1,6 +1,7 @@
 // import Line from "./Line.js";
 // import Rectangle from "./Rectangle.js";
 import SelectableMap from "../lib/SelectableMap.js";
+import Line from "./Line.js"
 
 export default class Tools extends SelectableMap{
     constructor(editor){
@@ -8,7 +9,10 @@ export default class Tools extends SelectableMap{
         this.editor = editor;
         this.toolName = '';
         // this.tools = new SelectableMap();
-        this.select('Line');
+
+        // this.select('Line');
+        // this.set(this.toolName, new Line(this.editor)); // line이 기본 툴이 된다.
+
     }
     select(toolName){
         if(!this.has(toolName)){
