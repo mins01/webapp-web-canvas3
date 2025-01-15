48d906c6164256aa93afb0cda1e23512d3d288bc
공대여자 <mins01.lycos.co.kr@gmail.com>
2025-01-15 23:51:18 +0900

    Document.Layers 개선

--- wc/element/Document/Layers.js
+++ wc/element/Document/Layers.js
@@ -1,20 +1,56 @@
 import SelectableArray from "../../lib/SelectableArray.js";
+import Canvas from "../Canvas.js";
 import Layer from "../Layer.js";
+import EllipseLayer from "../EllipseLayer.js";
+import RectangleLayer from "../RectangleLayer.js";
+import TextLayer from "../TextLayer.js";
 
-const Wc = { Layer };
+const Wc = { Canvas,Layer,EllipseLayer,RectangleLayer,TextLayer };
 
 export default class Layers extends SelectableArray{
     document = null;
     constructor(...elements) {
         super(...elements);
     }
+    add(layer){
+        const document = this.document
+        layer.parent = document;
+        const r = super.add(layer);
+        document.syncDrawLayer(layer);
+        layer.flush();
+        // document.flush();
+        document?.editor?.onselectLayer(document.layer);
+        return true;
+    }
+    remove(){
+        const document = this.document
+        super.remove();
+        document.syncDrawLayer(document.layer);
+        document.flush();
+        return true;
+    }
+    move(index){
+        const document = this.document
+        super.move(index);
+        document.flush();
+        document?.editor?.onselectLayer(document.layer);
+        return true;
+    }
+
+
+
     import(conf){
-        console.log(conf);
-        this.length = 0;
+        this.clear();
+        // console.log(this,conf);
+        // this.length = 0;
         conf.elements.forEach(layerConf => {
-            const layer = Wc.Layer.from(layerConf);
+            if(layerConf?.__class__===undefined){ throw new Error(`Module name is not exists. - ${layerConf.__class__}`); }
+            let module = Wc?.[layerConf.__class__]
+            if(!module){ throw new Error(`Module is not exists. - ${layerConf.__class__}`); }
+            const layer = module.from(layerConf);
             this.document.add(layer);
         });
+        if(conf?.selectedIndex !== undefined) this.select(conf.selectedIndex)
     }
     export(){
         return this.toJSON();
