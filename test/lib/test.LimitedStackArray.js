7c1f302274c47e2882a7da3eb1bf68df445f9ee5
mins01 <mins01.lycos.co.kr@gmail.com>
2025-01-16 11:04:18 +0900

    wc/lib/LimitedStackArray.js from

--- test/lib/test.LimitedStackArray.js
+++ test/lib/test.LimitedStackArray.js
@@ -1,7 +1,8 @@
 import LimitedStackArray from "../../wc/lib/LimitedStackArray.js";
 
 let r = null;
-let lsa = new LimitedStackArray(3)
+let lsa = null;
+lsa = new LimitedStackArray(3)
 lsa.push(0);console.assert(lsa.join(',')==='0', JSON.stringify(lsa))
 lsa.push(1);console.assert(lsa.join(',')==='0,1', JSON.stringify(lsa))
 lsa.push(2);console.assert(lsa.join(',')==='0,1,2', JSON.stringify(lsa))
@@ -12,4 +13,12 @@ r = lsa.pop();console.assert(lsa.join(',')==='' && r === 1, JSON.stringify(lsa))
 r = lsa.pop();console.assert(lsa.join(',')==='' && r === undefined, JSON.stringify(lsa))
 
 lsa.push(4,5,6,7,8,9);console.assert(lsa.join(',')==='7,8,9', JSON.stringify(lsa))
-console.log(lsa.join(','))
+
+lsa = new LimitedStackArray(3,1,2,3,4,5,6);
+console.log('toJSON',JSON.stringify(lsa))
+lsa = new LimitedStackArray(3);
+console.log('toJSON',JSON.stringify(lsa))
+lsa = LimitedStackArray.from(3,[1,2,3,4,5,6])
+console.log('toJSON',JSON.stringify(lsa))
+lsa = LimitedStackArray.from(3,[])
+console.log('toJSON',JSON.stringify(lsa))
--- wc/lib/LimitedStackArray.js
+++ wc/lib/LimitedStackArray.js
@@ -1,22 +1,22 @@
 export default class LimitedStackArray extends Array {
     maxLength = Infinity;
-    constructor(maxLength) {
-        if(!Number.isFinite(maxLength)){ throw new Error(`Limit must be a number. (${maxLength})`); }
-        super();
+    constructor(maxLength,...args) {
+        if(!Number.isFinite(maxLength)){ throw new Error(`MaxLength must be a number. (${maxLength})(${args.join()})`); }
+        super(...args);
         this.maxLength = maxLength;
+        this.limit()
     }
-
+    static from(maxLength,...args){
+        return new this(maxLength, ...Array.from(...args));
+    }
     push(...args) {
-        // if (this.length >= this.maxLength) {
-        //   this.shift();
-        // }
         Array.prototype.push.apply(this,args);
         this.limit()
         return this.length;
     }
 
     limit(){
-        if (this.length >= this.maxLength) {
+        if (this.length > this.maxLength) {
             let v = this.length - this.maxLength;
             if(v===1){
                 this.shift();
