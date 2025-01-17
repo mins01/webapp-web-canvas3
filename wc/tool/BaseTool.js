a7e83be5df5d197c6deac92035d227754e753e76
mins01 <mins01.lycos.co.kr@gmail.com>
2025-01-17 16:09:42 +0900

    getXyFromEvent, getXyInDocument, getXyInLayer

--- wc/tool/BaseTool.js
+++ wc/tool/BaseTool.js
@@ -62,48 +62,93 @@ export default class BaseTool {
 
 	}
 
+	getXyFromEvent(event){
+		let x = event.x + window.scrollX;
+		let y = event.y + window.scrollY;
+		return [x,y];
+	}
+
+	getXyInDocument(inX,inY){
+		const doc = this.document;
+		let x = inX - doc.offsetLeft;
+		let y = inY - doc.offsetTop;
+		const zoom = doc.zoom;
+		if(zoom!==1){
+			x += doc.width/2*(zoom-1);
+			y += doc.height/2*(zoom-1);
+			x = x/zoom;
+			y = y/zoom;
+		}
+		return [x,y];
+	}
+	getXyInLayer(inX,inY){
+		const layer = this.layer;
+		let x = inX - layer.left;
+		let y = inY - layer.top;
+		const zoom = layer.zoom;
+		if(zoom!==1){
+			x += layer.width/2*(zoom-1);
+			y += layer.height/2*(zoom-1);
+			x = x/zoom;
+			y = y/zoom;
+		}
+		return [x,y];
+	}
+
 	getXYForLayer(event){
 		const doc = this.document;
 		const layer = this.document.layer;
-		const docZoom = parseFloat(doc.zoom);
+		
 
 		let x = event.x - doc.offsetLeft - layer.left + window.scrollX;
 		let y = event.y - doc.offsetTop - layer.top + window.scrollY;
 
+		const docZoom = parseFloat(doc.zoom);
 		if(docZoom!==1){
-			const gLeft = doc.width/2*(docZoom-1)
-			const gTop = doc.height/2*(docZoom-1)
-			x += gLeft;
-			y += gTop;
+			x += doc.width/2*(docZoom-1);
+			y += doc.height/2*(docZoom-1);
 			x = x/docZoom;
 			y = y/docZoom;
 		}
-		const layerZoom = parseFloat(layer.zoom);
 
+		const layerZoom = parseFloat(layer.zoom);
 		if(layerZoom!==1){
-			const gLeft = layer.width/2*(layerZoom-1)
-			const gTop = layer.height/2*(layerZoom-1)
-			x += gLeft;
-			y += gTop;
+			x += layer.width/2*(layerZoom-1);
+			y += layer.height/2*(layerZoom-1);
 			x = x/layerZoom;
 			y = y/layerZoom;
 		}
 		return [x,y];
 	}
-	getXYForDocument(event){
-		const doc = this.document;
-		const zoom = parseFloat(doc.zoom);
-		let x = event.x - doc.offsetLeft + window.scrollX;
-		let y = event.y - doc.offsetTop + window.scrollY;
-		if(zoom!==1){
-			const gLeft = doc.width/2*(zoom-1)
-			const gTop = doc.height/2*(zoom-1)
-			x += gLeft;
-			y += gTop;
-			x = x/zoom;
-			y = y/zoom;
-		}
+	// getXYForDocument(event){
+	// 	const doc = this.document;
+	// 	const zoom = parseFloat(doc.zoom);
+	// 	let x = event.x - doc.offsetLeft + window.scrollX;
+	// 	let y = event.y - doc.offsetTop + window.scrollY;
+	// 	if(zoom!==1){
+	// 		const gLeft = doc.width/2*(zoom-1)
+	// 		const gTop = doc.height/2*(zoom-1)
+	// 		x += gLeft;
+	// 		y += gTop;
+	// 		x = x/zoom;
+	// 		y = y/zoom;
+	// 	}
 		
-		return [x,y];
+	// 	return [x,y];
+	// }
+
+	applyLayerAngle(ctx){
+		const doc = this.document;
+		const layer = this.document.layer;	
+		// doc.angle 적용
+		ctx.translate(doc.width / 2 - layer.left ,doc.height / 2 - layer.top)
+		ctx.rotate(-doc.angle*Math.PI)
+		ctx.translate(-(doc.width / 2 - layer.left) ,-(doc.height / 2 - layer.top))
+		// layer.angle 적용
+		ctx.translate(layer.width / 2,layer.height / 2)
+		ctx.rotate(-layer.angle*Math.PI)
+		ctx.translate(-layer.width / 2,-layer.height / 2)
+		// ctx.translate(- layer.left,- layer.top)
+
 	}
 }
