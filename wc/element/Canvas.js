a67e83ce66a0b0c602b88db6c7cb04e380ba19ca
mins01 <mins01.lycos.co.kr@gmail.com>
2025-01-17 11:09:26 +0900

    left,top getter/setter.  merge 버그 처리

--- wc/element/Canvas.js
+++ wc/element/Canvas.js
@@ -4,32 +4,12 @@ import Context2dUtil from "../lib/Context2dUtil.js";
 
 class Canvas extends HTMLCanvasElement{
     static context2dOptions = {"alpha":true,"antialias":true,"depth":true,"willReadFrequently": false,};
+
     static get keys(){
         return ['width', 'height', 'drawable', 'label', 'contextConfig', 'createdAt','updatedAt',];
     }
+    // static get observedAttributes() { return ['width', 'height']; }
 
-    ctx = null;
-    drawable = true;
-    constructor(w=null,h=null){
-        super();
-
-        this.drawable = true; // 그리기 가능한가? 그리기 툴에서 체크.
-        if(this.id === undefined || this.id === '') this.id =  'wc-'+this.constructor.name.toLocaleLowerCase()+'-'+this.constructor.getIdCounter()+'-'+(Math.floor(Math.random()*1000000)).toString().padStart(6,'0');
-        this.label = "created at "+(new Date()).toLocaleString(['ko'],{dateStyle:'medium',timeStyle:'medium',hourCycle:'h24'}).replace(/[^\d]/,'');
-        this.contextConfig = new Context2dConfig();
-        
-        Object.defineProperty(this,'ctx',{ enumerable: false, configurable: true, writable: true, value: null, })
-        Object.defineProperty(this, 'parent', { enumerable: false, configurable: true, writable: true, value: null, })
-        // this.parent = null
-
-        this.createdAt = Date.now();
-        this.updatedAt = Date.now();
-        this.setContext2d();
-
-        if(w && w != this.width) this.width = w;
-        if(h && h != this.height) this.height = h;        
-    }
-    
     static getIdCounter(){
         if(Canvas._idCounter === undefined){ Canvas._idCounter = 0;}
         return ++ Canvas._idCounter;
@@ -39,10 +19,39 @@ class Canvas extends HTMLCanvasElement{
         window.customElements.define(name, this,{ extends: "canvas" });
     }
 
-    static get observedAttributes() { return ['width', 'height']; }
+
+    ctx = null;
+    drawable = true;
+    #left;
+    #top;
+    constructor(w=null,h=null){
+        super();
+
+        this.drawable = true; // 그리기 가능한가? 그리기 툴에서 체크.
+        if(this.id === undefined || this.id === '') this.id =  'wc-'+this.constructor.name.toLocaleLowerCase()+'-'+this.constructor.getIdCounter()+'-'+(Math.floor(Math.random()*1000000)).toString().padStart(6,'0');
+        this.label = "created at "+(new Date()).toLocaleString(['ko'],{dateStyle:'medium',timeStyle:'medium',hourCycle:'h24'}).replace(/[^\d]/,'');
+        this.contextConfig = new Context2dConfig();
+
+        Object.defineProperty(this,'ctx',{ enumerable: false, configurable: true, writable: true, value: null, })
+        Object.defineProperty(this, 'parent', { enumerable: false, configurable: true, writable: true, value: null, })
+        // this.parent = null
+
+        this.createdAt = Date.now();
+        this.updatedAt = Date.now();
+        this.setContext2d();
+
+        if(w && w != this.width) this.width = w;
+        if(h && h != this.height) this.height = h;
+    }
+
+
+
+
+
+
 
     connectedCallback(){
-        
+
     }
     disconnectedCallback(){
 
@@ -59,7 +68,7 @@ class Canvas extends HTMLCanvasElement{
         this.ctx = this.getContext2d(options)
         return this.ctx;
     }
-    getContext2d(options=Canvas.context2dOptions){       
+    getContext2d(options=Canvas.context2dOptions){
         return this.getContext('2d',options)
     }
 
@@ -71,20 +80,20 @@ class Canvas extends HTMLCanvasElement{
     }
 
     /**
-     * @deprecated 
+     * @deprecated
      */
     ctxCommand(){
         let inArgs = [...arguments];
         const method = inArgs[0]??null;
         const args = (inArgs??[]).slice(1);
         // console.log(method,args,this);
-        if (typeof this.ctx[method] === "function") { 
+        if (typeof this.ctx[method] === "function") {
             this.ctx[method].apply(this.ctx,args);
         }else{
             console.error('error: ctxCommand',inArgs);
         }
         // console.log(method);
-        
+
     }
 
     draw(){ // 따로 그리기 동작이 있을 경우.
@@ -99,7 +108,7 @@ class Canvas extends HTMLCanvasElement{
     sync(){
         this.parentFlush();
     }
-    
+
     parentFlush(){
         this?.parent?.flush();
     }
@@ -119,33 +128,7 @@ class Canvas extends HTMLCanvasElement{
         this.ctx.restore();
     }
 
-    merge(canvas){
-        const xmin = Math.min(this.left,canvas.left)
-        const ymin = Math.min(this.top,canvas.top)
-        const xmax = Math.max(this.left+this.width,canvas.left+canvas.width)
-        const ymax = Math.max(this.top+this.height,canvas.top+canvas.height)
-        const w = xmax-xmin;
-        const h = ymax-ymin;
 
-        
-        const imageData = this.ctx.getImageData(0,0,this.width,this.height);
-        let dx = this.left-xmin;
-        let dy = this.top-ymin;
-        
-        this.left = xmin;
-        this.top = ymin;
-        this.width = w;
-        this.height = h;
-        // this.fill('#00ee0033');
-
-        this.ctx.putImageData(imageData,dx,dy);
-
-        dx = canvas.left-xmin;
-        dy = canvas.top-ymin;
-        this.ctx.drawImage(canvas, dx, dy, canvas.width, canvas.height);        
-
-        // this.flush();
-    }
     clone(){
         const newCanvas = new this.constructor(this.width,this.height);
         Object.assign(newCanvas,this);
@@ -162,9 +145,6 @@ class Canvas extends HTMLCanvasElement{
         this.height = height;
         this.ctx.drawImage(cloned, 0, 0, width, height);
     }
-
-
-
 
 
 
--- wc/element/Layer.js
+++ wc/element/Layer.js
@@ -7,15 +7,20 @@ export default class Layer extends Canvas{
     static get keys(){
         return super.keys.concat(['left', 'top','compositeOperation','alpha','angle','zoom'])
     }
-
+    static defineCustomElements(name='wc-layer'){
+        super.defineCustomElements(name);
+    }
+    
     // x = null;
     // y = null;
-    left = null;
-    top = null;
+    // left = null;
+    // top = null;
     compositeOperation = null;
     alpha = null;
     zoom = null;
     angle = null;
+    #left;
+    #top;
     constructor(w=null,h=null){
         super(w,h);
         
@@ -26,19 +31,36 @@ export default class Layer extends Canvas{
         this.zoom = 1;
         this.angle = 0;
     }
-    static defineCustomElements(name='wc-layer'){
-        super.defineCustomElements(name);
-    }
-        
-    // get x(){ return this._x; }
-    // set x(x){ this._x = x; }
-    // get y(){ return this._y; }
-    // set y(y){ this._y = y; }
 
-    // get compositeOperation(){ return this._compositeOperation; }
-    // set compositeOperation(compositeOperation){ this._compositeOperation = compositeOperation; }
-    // get opacity(){ return this._opacity; }
-    // set opacity(opacity){ this._opacity = opacity; this.flush(); }
-    // get alpha(){ return this._alpha; }
-    // set alpha(alpha){ this._alpha = alpha; }
+    get left(){ return this.#left; }
+    get top(){ return this.#top; }
+    set left(v){ this.#left = parseFloat(v); this.setAttribute('left',this.#left) }
+    set top(v){ this.#top = parseFloat(v); this.setAttribute('top',this.#top) }
+
+    merge(canvas){
+        const xmin = Math.min(this.left,canvas.left)
+        const ymin = Math.min(this.top,canvas.top)
+        const xmax = Math.max(this.left+this.width,canvas.left+canvas.width)
+        const ymax = Math.max(this.top+this.height,canvas.top+canvas.height)
+        const w = xmax-xmin;
+        const h = ymax-ymin;
+
+        const imageData = this.ctx.getImageData(0,0,this.width,this.height);
+        let dx = this.left-xmin;
+        let dy = this.top-ymin;
+
+        this.left = xmin;
+        this.top = ymin;
+        this.width = w;
+        this.height = h;
+        // this.fill('#00ee0033');
+
+        this.ctx.putImageData(imageData,dx,dy);
+
+        dx = canvas.left-xmin;
+        dy = canvas.top-ymin;
+        this.ctx.drawImage(canvas, dx, dy, canvas.width, canvas.height);
+
+        // this.flush();
+    }
 }
