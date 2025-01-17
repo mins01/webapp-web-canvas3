879dc92aa35a9cb1d6ed0997cceb16a007946afa
mins01 <mins01.lycos.co.kr@gmail.com>
2025-01-17 10:33:33 +0900

    wc/lib/LimitedQueueArray.js 잡다

--- wc/lib/LimitedQueueArray.js
+++ wc/lib/LimitedQueueArray.js
@@ -1,4 +1,18 @@
+
+/**
+ * push 후 size가 넘어가면 자동으로 앞의 요소부터 버리는 배열
+ *
+ * @export
+ * @class LimitedQueueArray
+ * @typedef {LimitedQueueArray}
+ * @extends {Array}
+ */
 export default class LimitedQueueArray extends Array {
+    /**
+     * 배열의 제한 길이
+     *
+     * @type {number}
+     */
     size = Infinity;
     constructor(size,...args) {
         if(!Number.isFinite(size) || size<=0){ throw new Error(`Size must be a positive integer. (${size})`); }
@@ -15,16 +29,22 @@ export default class LimitedQueueArray extends Array {
         return this.length;
     }
 
+    /**
+     * Description placeholder
+     *
+     * @returns {number} 삭제된 요소의 수
+     */
     limit(){
         if (this.length > this.size) {
             let v = this.length - this.size;
             if(v===1){
                 this.shift();
             }else{
-                let v = this.length - this.size;
                 this.splice(0,v);
             }
+            return v;
         }
+        return 0;
     }
     toJSON(){
         return {
