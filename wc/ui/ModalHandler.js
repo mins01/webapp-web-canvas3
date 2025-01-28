a4c714ec72465165d99ee700aa435a30ac91dd53
공대여자 <mins01.lycos.co.kr@gmail.com>
2025-01-28 18:10:07 +0900

    wc/ui/ModalHandler.js

--- wc/ui/ModalHandler.js
+++ wc/ui/ModalHandler.js
@@ -1,8 +1,22 @@
-export default class ModalHandler{
-  modals = null;
-  layer = null;
+export default class ModalHandler extends Map{
   constructor(){
-    this.modals = {}
+    super(...arguments);
   }
 
+  windowOnload(){
+    globalThis.window.addEventListener('load',(event)=>{
+      this.collectModals()
+    })
+  }
+  collectModals(){
+    globalThis.document.querySelectorAll('.modal[id]').forEach(node=>{
+        const modal = new bootstrap.Modal(node);
+        if(!this.has(node.id)) this.set(node.id,modal);
+    })   
+  }
+  hideAll(){
+    this.forEach((modal,k)=>{
+      modal.hide();
+    })
+  }
 }
