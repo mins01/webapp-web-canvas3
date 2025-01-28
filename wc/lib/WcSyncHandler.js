b453c3390aea7cf79a0690b67cbe46cd08d6afb3
공대여자 <mins01.lycos.co.kr@gmail.com>
2025-01-28 20:34:36 +0900

    wc/lib/WcSyncHandler.js 적용중

--- wc/lib/WcSyncHandler.js
+++ wc/lib/WcSyncHandler.js
@@ -1,41 +1,42 @@
-export default class WcSyncHandler{
+export default class WcSyncFromHandler{
   constructor(){
 
   }
 
   addEventListener(){
-    window.addEventListener('change',(event)=>{
-      const target = event.target;
-      if(target.dataset.wcSync){ this.sync(target) }
-    })
-    window.addEventListener('input',(event)=>{
-      const target = event.target;
-      if(target.dataset.wcSync){ this.sync(target) }
-    })
+    // window.addEventListener('change',(event)=>{
+    //   const target = event.target;
+    //   if(target.dataset.wcSyncFrom){ this.syncFrom(target) }
+    // })
+    // window.addEventListener('input',(event)=>{
+    //   const target = event.target;
+    //   if(target.dataset.wcSyncFrom){ this.syncFrom(target) }
+    // })
     window.addEventListener('show.bs.modal',(event)=>{
       const target = event.target;
-      target.querySelectorAll('[data-wc-sync]').forEach(el=> {
-        this.sync(el);
+      target.querySelectorAll('[data-wc-sync-from]').forEach(el=> {
+        this.syncFrom(el);
       });      
     })
   }
 
-  sync(input){
-    const wcSync = input.dataset.wcSync;
-    const splits = wcSync.split(/\./g);
+  syncFrom(input){
+    const wcSyncFrom = input.dataset.wcSyncFrom;
+    const splits = wcSyncFrom.split(/\./g);
     const inputType = input.type??'text'
 
     let v = globalThis;
     splits.forEach((split)=>{
       if(v===null){return;}
-      if(!v[split]){ v = null}
-      v = v[split];
+      v = v?.[split]??null;
     })
+    if(v===null){
+      return;
+    }
     if(['checkbox','radio'].includes(inputType)){
       if(input.value == v) input.checked = true;
     }else{
       input.value = v
     }
-    
   }
 }
