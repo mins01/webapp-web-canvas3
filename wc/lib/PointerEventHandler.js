c3f6d3e179d54d8dcd0d16e0fd0f454fed775b8d
공대여자 <mins01.lycos.co.kr@gmail.com>
2025-07-31 23:14:01 +0900

    {passive: false} 적용. 성능 향상은 모르겠다.

--- wc/lib/PointerEventHandler.js
+++ wc/lib/PointerEventHandler.js
@@ -13,15 +13,16 @@ export default class PointerEventHandler{
         this.downAt = null;
     }
     addEventListener(target){
+        this.target = target
         target.addEventListener('pointerdown',this.pointerdown);
-        window.addEventListener('pointermove',this.pointermove)
+        window.addEventListener('pointermove',this.pointermove,{passive: false})
         window.addEventListener('pointerup',this.pointerup)
         window.addEventListener('pointerleave',this.pointerup)
         window.addEventListener('pointercancel',this.pointerup)
     }
     removeEventListener(){
-        this.target.removeEventListener('pointerdown',this.pointerdown);
-        window.removeEventListener('pointermove',this.pointermove)
+        if(this.target) this.target.removeEventListener('pointerdown',this.pointerdown);
+        window.removeEventListener('pointermove',this.pointermove,{ passive: false })
         window.removeEventListener('pointerup',this.pointerup)
         window.removeEventListener('pointerleave',this.pointerup)
         window.removeEventListener('pointercancel',this.pointerup)
@@ -46,20 +47,22 @@ export default class PointerEventHandler{
     }
     pointermove = (event)=>{
         if(!this.checkInputMode(event)){ return; }
-        event.stopPropagation();
-		event.preventDefault();
         if(this.downAt){
+            event.stopPropagation();
+            event.preventDefault();
             if(this.pointers.has(event.pointerId)) this.pointers.set(event.pointerId, { x: event.clientX, y: event.clientY });
             if(this.onpointermove){this.onpointermove(event)}
         }
     }
     pointerup = (event)=>{
         if(!this.checkInputMode(event)){ return; }
-        event.stopPropagation();
-		event.preventDefault();
-        this.pointers.delete(event.pointerId);
-        if(this.onpointerup){this.onpointerup(event)}
-        if(this.pointers.size===0){this.downAt = null; this.maxPointers=0;}
+        if(this.downAt){
+            event.stopPropagation();
+            event.preventDefault();
+            this.pointers.delete(event.pointerId);
+            if(this.onpointerup){this.onpointerup(event)}
+            if(this.pointers.size===0){this.downAt = null; this.maxPointers=0;}
+        }
     }
 
     onpointerdown(event){
