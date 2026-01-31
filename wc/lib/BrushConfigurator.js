a8cfed86ad8bbac159d115dc7319c0300381764a
공대여자 <mins01.lycos.co.kr@gmail.com>
2026-01-31 20:49:40 +0900

    BrushConfigurator.crayon spacing 0.5

--- wc/lib/BrushConfigurator.js
+++ wc/lib/BrushConfigurator.js
@@ -141,7 +141,7 @@ class BrushConfigurator{
   static crayon(brush){
     brush.brushConfig.reset();
     brush.setBrushConfig(
-      {"compositeOperation":"source-over","shape":"#brush-shape-brush-04","size":30,"angle":0,"roundness":1,"scaleX":1,"scaleY":"1","flipX":false,"flipY":false,"hardness":1,"spacing":0.25,"sizeJitter":0,"sizeControl":"off","mininumSizeRatio":0.01,"angleJitter":1,"angleControl":"off","roundnessJitter":0,"roundnessControl":"off","mininumRoundness":0.1,"scaleYControl":"off","mininumScaleY":0.1,"opacity":1,"opacityJitter":0,"opacityControl":"off","mininumOpacity":0.1,"flow":1,"flowJitter":0,"flowControl":"off","mininumFlow":"0.1","hueJitter":0,"saturationJitter":0,"brightnessJitter":0,"scatterAmount":0,"scatterAxes":"y","scatterCount":2,"scatterCountJitter":0,"flattenOpacity":true,"buildUpInterval":0}
+      {"compositeOperation":"source-over","shape":"#brush-shape-brush-04","size":30,"angle":0,"roundness":1,"scaleX":1,"scaleY":"1","flipX":false,"flipY":false,"hardness":1,"spacing":0.5,"sizeJitter":0,"sizeControl":"off","mininumSizeRatio":0.01,"angleJitter":1,"angleControl":"off","roundnessJitter":0,"roundnessControl":"off","mininumRoundness":0.1,"scaleYControl":"off","mininumScaleY":0.1,"opacity":1,"opacityJitter":0,"opacityControl":"off","mininumOpacity":0.1,"flow":1,"flowJitter":0,"flowControl":"off","mininumFlow":"0.1","hueJitter":0,"saturationJitter":0,"brightnessJitter":0,"scatterAmount":0,"scatterAxes":"y","scatterCount":2,"scatterCountJitter":0,"flattenOpacity":true,"buildUpInterval":0}
     )
     brush.saveBrushConfig();
     brush.flush();
